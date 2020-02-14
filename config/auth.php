289bffe1514ae8733b0a2b3c6beb43d6b75d1c67
kiran <kumar.alg2@gmail.com>
2020-02-14 17:49:06 +0530

    Adding the Token Authentication using Passport

--- config/auth.php
+++ config/auth.php
@@ -37,14 +37,14 @@ return [
 
     'guards' => [
         'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
-
-        'api' => [
-            'driver' => 'token',
-            'provider' => 'users',
-        ],
+            'driver' => 'session', 
+            'provider' => 'users', 
+        ], 
+    
+        'api' => [ 
+            'driver' => 'passport', 
+            'provider' => 'users', 
+        ], 
     ],
 
     /*
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+              //Add api_token field
+            $table->string('api_token', 60)->unique()
+            ->nullable()
+            ->default(null);
             $table->rememberToken();
             $table->timestamps();
         });
